9eb7fbcab9d242daab3255cbca2809fb2063ca5d
Andrew <andrew.r.pedelty@gmail.com>
2013-03-11 16:48:34 -0700

    Create dedict.py

---
+++ dedict.py
@@ -0,0 +1,14 @@
+!#/usr/bin/env python2.7
+
+""" Takes a nested dict structure and turns it into a list of dicts, 
+    merging the old highest-level keys into the lower dicts. 
+"""
+
+def dedict(adict, new_key): 
+    lst = []
+    for k,v in adict.items(): 
+        v[new_key] = k
+        lst.append(v)
+    return lst
+
+
