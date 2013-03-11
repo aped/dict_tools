c6fa4df8cb8ac48d888940e13acb0e368d54c91b
Andrew <andrew.r.pedelty@gmail.com>
2013-03-11 16:09:23 -0700

    Update rekey.py

--- rekey.py
+++ rekey.py
@@ -4,10 +4,10 @@
 
 
 def rekey(to_rekey, newmap): 
-""" Where newmap is a dict itself, from old values to new values, or
-    a function.
-    Keys in to_rekey with no correlate in the map will be preserved. 
-"""
+    """ Where newmap is a dict itself, from old values to new values, or
+        a function.
+        Keys in to_rekey with no correlate in the map will be preserved. 
+    """
     if hasattr(newmap, "__contains__"): 
         return {newmap[k]:v for k,v in to_rekey if k in newmap else k:v}
     else: 
