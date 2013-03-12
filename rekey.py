bb038055ca386f7fe31f3b27a5cafb2c94c2d279
Andrew <andrew.r.pedelty@gmail.com>
2013-03-11 17:21:27 -0700

    Update rekey.py

--- rekey.py
+++ rekey.py
@@ -12,3 +12,11 @@ def rekey(to_rekey, newmap):
         return {(newmap[k] if k in newmap else k):v for k,v in to_rekey.items()}
     else: 
         return {newmap(k):v for k,v in to_rekey.items()}
+
+
+def rekey_ugly(to_rekey, newmap): 
+    """ Same as above, but with a more functional feel. For fun and
+    profit. or something. Misses some functionality. 
+    """
+    op = "get" if issubclass(type(newmap), dict) else "__call__")
+    return {(getattr(newmap, op)(k):v for k,v in to_rekey.items()}
